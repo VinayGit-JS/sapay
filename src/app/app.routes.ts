d4d9db18593d4394e233dd61890615a8d98eb8a4
VikashMlv <viky.mlv8@gmail.com>
2024-12-26 19:57:31 +0530

    privacy policy added and footer content changed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,7 @@ import { AboutComponent } from './features/about/about.component';
 import { ContactComponent } from './features/contact/contact.component';
 import { RefundPolicyComponent } from './features/refund-policy/refund-policy.component';
 import { NotFoundComponent } from './features/not-found/not-found.component';
+import { PrivacyPolicyComponent } from './features/privacy-policy/privacy-policy.component';
 
 // Define the application routes
 export const appRoutes: Routes = [
@@ -12,6 +13,7 @@ export const appRoutes: Routes = [
     { path: 'about', component: AboutComponent },  // About page route
     { path: 'contact', component: ContactComponent },  // contact page route
     { path: 'refund-policy', component: RefundPolicyComponent },  // refund policy route
+    { path: 'privacy-policy', component: PrivacyPolicyComponent },  // refund policy route
     { path: '**', component: NotFoundComponent },  // not found route if any route not match
 
 
