ecc293e5535e8f7a7f4d747f033bab202a7f5815
VinayGit-JS <logicfearedu@gmail.com>
2024-12-26 16:12:14 +0530

    changes

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,10 +1,9 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from "../../shared/components/header/header.component";
-import { FooterComponent } from "../../shared/components/footer/footer.component";
+import { CarouselModule } from 'ngx-bootstrap/carousel';
 
 @Component({
   selector: 'app-home',
-  imports: [HeaderComponent, FooterComponent],
+  imports: [CarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
